a2b6a49e1c09fc9481df4ce2afcc2b21be5cdef2
Nicholas Brandao <nicholas.computacao@outlook.com>
2021-01-17 16:23:11 -0300

    Add prettier and magic sdk

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,27 @@
 // tslint:disable-next-line
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
+const Magic = require('mtgsdk-ts');
+const _ = require('lodash');
 
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+function getCardInfo(card: object) {
+  const model = [
+    'name',
+    'manaCost',
+    'cmc',
+    'colorIdentity',
+    'imageUrl',
+    'multiverseid',
+  ];
+  return _.pick(card, model);
+}
 
-app.listen( port );
+const callCards = async (req: any, res: any) => {
+  const cards = await Magic.Cards.where({ name: 'nicol' });
+  const filteredInfo = await Promise.all(await cards.map(getCardInfo));
+  res.send({ cards: filteredInfo });
+};
+
+app.get('/', callCards);
+app.listen(port);
